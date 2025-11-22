c0f21c35f5fafaaeea592bc8db89977b64c17716
Mike-zdev <mikezwinner@gmail.com>
2025-11-22 20:16:44 +0300

    Update backend API base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // make sure backend is on this port
+  baseURL: "https://task-manager-backend-33pj.onrender.com/api",
 });
 
 export default api;
